02e78345536a7d3d7aa1646d1f408314de1a6eca
behan05 <behankrbth@outlook.com>
2024-03-24 00:19:58 +0530

    header complete

---
+++ public/assets/js/script.js
@@ -0,0 +1,31 @@
+// document.addEventListener("DOMContentLoaded", () => {
+
+//     const menuBtn = document.querySelector(".container__menuBtn");
+//     const closeBtn = document.querySelector(".container__closeBtn");
+//     const moon = document.querySelector(".container__moon");
+//     const body = document.querySelector("body");
+//     // console.log(closeBtn);
+
+//     if (menuBtn) {
+//         menuBtn.addEventListener('click', (e) => {
+//             menuBtn.style.display = "none"
+//             closeBtn.style.display = "block"
+//             menuBtn.style
+//         })
+//     }
+//     if (closeBtn) {
+//         closeBtn.addEventListener('click', (e) => {
+//             menuBtn.style.display = "block"
+//             closeBtn.style.display = "none"
+//         })
+//     }
+//     if (moon) {
+//         moon.addEventListener("click", () => {
+//             moon.style.color = "red"
+//             body.style.backgroundColor = "black"
+//             body.style.color = "white"
+
+//         })
+//     }
+
+// })
